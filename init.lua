47cb262fc420d08de5527997481da4117b0e2cdd
Gleb Beloded <gleb.beloded@softswiss.com>
2023-07-16 21:52:18 +0200

    use new file tree mappings api

--- init.lua
+++ init.lua
@@ -14,7 +14,6 @@ require("gleb.autopairs") -- must be after treesitter
 require("gleb.comments") -- must be after treesitter
 require("gleb.git")
 require("gleb.debugging")
-require("gleb.nvim-tree")
 require("gleb.bufferline")
 require("gleb.project")
 require("gleb.indentline")
@@ -23,3 +22,4 @@ require("gleb.autocommands")
 require("gleb.keymaps")
 require("gleb.colors")
 require("gleb.lualine") -- try to put after plugins that create splits
+require("gleb.file_tree")
--- lua/gleb/file_tree/init.lua
+++ lua/gleb/file_tree/init.lua
@@ -3,14 +3,8 @@ if not status_ok then
 	return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-	return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
 nvim_tree.setup({
+	on_attach = require("gleb.file_tree.keymaps"),
 	actions = {
 		open_file = {
 			quit_on_open = true, -- hide tree after opening file
@@ -77,13 +71,6 @@ nvim_tree.setup({
 	view = {
 		width = 30,
 		side = "left",
-		mappings = {
-			list = {
-				{ key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
-				{ key = "h", cb = tree_cb("close_node") },
-				{ key = "v", cb = tree_cb("vsplit") },
-				{ key = "i", cb = tree_cb("vsplit") },
-			},
-		},
+		mappings = {},
 	},
 })
